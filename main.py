76084fe62acc22add224ffdf862358de1bafafd0
Teun <vanwezelteun@gmail.com>
2021-05-11 16:51:13 +0200

    Add BoidFrame class

--- main.py
+++ main.py
@@ -8,38 +8,29 @@ import math
 
 # class files
 from boid import Boid
+from boidframe import BoidFrame
 
-pygame.init()
-
-pi = math.pi
-
-width = 512
-height = 512
+width = 1024
+height = 1024
 
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((width, height))
 
-red = pygame.Color(255, 0, 0)
+frame = BoidFrame()
 
-boid_list = []
+for i in range(1000):
+    frame.add_boid(Boid((500,500), color=Color(random.randint(0, 255), random.randint(0,255), 28), size=8, velocity=(Vector2(random.uniform(-0.8, 0.8), random.uniform(-0.3, 0.7)))))
 
-for i in range(20):
-    boid_list.append(Boid((500,500), velocity=(Vector2(random.uniform(0.2, 0.8), random.uniform(0.3, 0.7)))))
-
-# boid = Boid((500, 500), velocity=Vector2(0.5, -0.25))
-# boid = Boid((550, 100), velocity=Vector2(0, -5)).draw(screen)
-# boid = Boid((600, 600), velocity=Vector2(0.33, 4)).draw(screen)
-# boid = Boid((650, 410), velocity=Vector2(1, 1)).draw(screen)
-# boid = Boid((700, 990), velocity=Vector2(-1, 2)).draw(screen)
-# boid = Boid((750, 220), velocity=Vector2(-4, 0)).draw(screen)
-
+pygame.init()
 while True:
     dt = clock.tick(60)
     screen.fill(Color(0,0,0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-    for boid in boid_list:
-        boid.do_step(dt)
+    frame.do_step(dt)
+    for boid in frame.boid_list:
         boid.draw(screen)
+    frame.update_average_boid_pos()
+    print(frame.average_boid_pos)
     pygame.display.update()
