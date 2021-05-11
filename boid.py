05de67a2eeb2a0f002e5a7aa60a941d0476ffd92
Teun <vanwezelteun@gmail.com>
2021-05-11 15:56:30 +0200

    Add drawing/movement to boids

--- boid.py
+++ boid.py
@@ -1,16 +1,42 @@
+import copy
+import random
+import pygame
+from pygame import Color
+from pygame.math import Vector2
+
 class Boid:
-  def __init__(self): # window as input aswell??
-    self.pos = (random.random , )
-    self.speed = random.random(0.9, 1)
-    self.angle = random.random(0.0, 2*math.pi)
-    self.time = random.random(0.0, 10000) # perlin noise for angle noise?
+  def __init__(self, 
+                pos=Vector2(0, 0),
+                size=16,
+                velocity=Vector2(1,1),
+                color=Color(255,255,255)): # window as input aswell??
+    self.pos = pos
+    self.size = size
+    self.velocity = velocity
+    self.color = color
+    # self.time = random.random(0.0, 10000) # perlin noise for angle noise?
 
+  def draw(self, surface):
+    # draw boid with pygame
+    s = self.size * self.velocity.normalize() # use the normalized velocity vector to draw the head
+    head = self.pos + s
+    # makes a copy of s rotated 90 degrees to the right
+    # deepcopy is used to prevent unintended changes to s
+    s_right = copy.deepcopy(s) 
+    s_right.x, s_right.y = s_right.y, -s_right.x 
+    foot_1 = self.pos - s + 0.5 * s_right
+    # same thing, but rotated 90 degrees to the left
+    s_left = copy.deepcopy(s)
+    s_left.x , s_left.y = -s_left.y, s_left.x 
+    foot_2 = self.pos - s + 0.5 * s_left
+    pygame.draw.polygon(surface, self.color, (head, foot_1, foot_2))
 
-  def draw(self):
-    # draw with pygame
-    print("") # error otherwise, can be deleted when other code is written
+  def do_step(self, dt):
+    self.pos = self.pos + dt * self.velocity
+    self.pos.x = self.pos.x % 512
+    self.pos.y = self.pos.y % 512
 
-  def changeAngle():
+  def changeAngle(self):
     # self.angle += perlin(self.time)
     print("") # error otherwise, can be deleted when other code is written
  
--- main.py
+++ main.py
@@ -1,28 +1,45 @@
 import pygame
+from pygame.math import Vector2
+from pygame import Color
 import sys
 import random
 import math
 
+
 # class files
-import boid
+from boid import Boid
 
 pygame.init()
 
 pi = math.pi
 
-width = 1300
-height = 700
+width = 512
+height = 512
 
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((width, height))
 
 red = pygame.Color(255, 0, 0)
 
-# game loop
+boid_list = []
+
+for i in range(20):
+    boid_list.append(Boid((500,500), velocity=(Vector2(random.uniform(0.2, 0.8), random.uniform(0.3, 0.7)))))
+
+# boid = Boid((500, 500), velocity=Vector2(0.5, -0.25))
+# boid = Boid((550, 100), velocity=Vector2(0, -5)).draw(screen)
+# boid = Boid((600, 600), velocity=Vector2(0.33, 4)).draw(screen)
+# boid = Boid((650, 410), velocity=Vector2(1, 1)).draw(screen)
+# boid = Boid((700, 990), velocity=Vector2(-1, 2)).draw(screen)
+# boid = Boid((750, 220), velocity=Vector2(-4, 0)).draw(screen)
 
 while True:
+    dt = clock.tick(60)
+    screen.fill(Color(0,0,0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-    pygame.draw.circle(screen, red, (200, 200), 100)
+    for boid in boid_list:
+        boid.do_step(dt)
+        boid.draw(screen)
     pygame.display.update()
