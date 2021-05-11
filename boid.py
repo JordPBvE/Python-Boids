32347366882d51f9c8d8c6cfb564c4943c17a620
Jord van Eldik <jord.vaneldik@gmail.com>
2021-05-11 12:14:06 +0200

    boid.py function organisation

--- boid.py
+++ boid.py
@@ -1,19 +1,30 @@
 class Boid:
-  def __init__(self): # window as input aswell??
-    self.pos = (random.random , )
-    self.speed = random.random(0.9, 1)
-    self.angle = random.random(0.0, 2*math.pi)
-    self.time = random.random(0.0, 10000) # perlin noise for angle noise?
+    def __init__(self): # window as input aswell??
+        self.pos = (random.random , )
+        self.velocity = random.random(0.9, 1)
+        self.time = random.random(0.0, 10000) # perlin noise for angle noise?
 
 
-  def draw(self):
-    # draw with pygame
-    print("") # error otherwise, can be deleted when other code is written
+    def draw(self):
+        # draw with pygame
+        print("") # error otherwise, can be deleted when other code is written
 
-  def changeAngle():
-    # self.angle += perlin(self.time)
-    print("") # error otherwise, can be deleted when other code is written
- 
+    def changeVelocity(self, boidArray):
+        # self.angle += perlin(self.time)
+        v1 = rule1(self, boidArray)
+        v2 = rule2(self, boidArray)
+        v3 = rule3(self, boidArray)
+
+        self.velocity = self.velocity + v1 + v2 + v3
+
+    def rule1():
+        print("") 
+    def rule2():
+        print("")
+    def rule3():
+        print("")
+
+
 ### time input for noise function
   # def timeChange(self):
   #   self.time += 1
