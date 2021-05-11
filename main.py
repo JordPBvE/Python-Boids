f9fa9100489a69773781e62267aa6d977d4e0618
Teun <vanwezelteun@gmail.com>
2021-05-11 12:27:21 +0200

    Draw circle

--- main.py
+++ main.py
@@ -16,10 +16,13 @@ height = 700
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((width, height))
 
+red = pygame.Color(255, 0, 0)
+
 # game loop
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+    pygame.draw.circle(screen, red, (200, 200), 100)
     pygame.display.update()
